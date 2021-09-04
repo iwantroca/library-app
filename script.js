14aa8ac24e90be0e1fdb890a5b0ecdf4c87d2471
Iwantroca <mohitnotiyal@yahoo.com>
2021-09-04 18:58:39 +0530

    Add console based working

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const addBookBtn = document.querySelector("#add-book");
 
 let myLibrary = [];
+
+function Book(name, author, pages) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+}
+const book1 = new Book("fdk", "fdfdl", 222);
+myLibrary.push(book1);
+console.log(myLibrary);
+
+let bookTitle = document.querySelector("#book-title input");
+function getBookName() {}
