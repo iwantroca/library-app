13b3b2aee982d132fc33b0f60e51a45fbd756ee2
Iwantroca <mohitnotiyal@yahoo.com>
2021-09-05 15:50:41 +0530

    Fix modal

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ const closeBtn = document.querySelector(".fa-xmark");
 const addBookModal = document.querySelector("#addBookModal");
 const submitFormBtn = document.querySelector("#submit-btn");
 const bookTitle = document.querySelector("#book-title");
+const bookAuthor = document.querySelector("#book-author");
+const bookPages = document.querySelector("#book-pages");
+const readCheck = document.querySelector("#read-check");
 
 let myLibrary = [];
 let book = null;
 
-function Book(name, author, pages) {
+function Book(name, author, pages, readStatus) {
   this.name = name;
   this.author = author;
   this.pages = pages;
+  this.readStatus = readStatus;
 }
 function makeModalInvisible() {
   addBookModal.style.display = "none";
@@ -18,11 +22,19 @@ function makeModalInvisible() {
 function makeModalVisible() {
   addBookModal.style.display = "block";
 }
+function submitForm() {
+  if (bookTitle.value && bookAuthor.value && bookPages.value) {
+    book = new Book(
+      bookTitle.value,
+      bookAuthor.value,
+      bookPages.value,
+      readCheck.checked
+    );
+    myLibrary.push(book);
+    makeModalInvisible();
+  }
+}
 
 addBookBtn.addEventListener("click", makeModalVisible);
 closeBtn.addEventListener("click", makeModalInvisible);
-submitFormBtn.addEventListener("click", () => {
-  book = new Book(bookTitle.value, 33, 33);
-  myLibrary.push(book);
-  makeModalInvisible();
-});
+submitFormBtn.addEventListener("click", submitForm);
