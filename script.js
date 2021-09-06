2a141ff6b89428b868990db674ecdbc74915b7cc
Iwantroca <mohitnotiyal@yahoo.com>
2021-09-06 19:12:22 +0530

    Add delete button on book card

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const bookInputs = document.querySelectorAll(".book-detail");
 const bookContainer = document.querySelector("#books-container");
 
 let myLibrary = [];
+let bookDelBtn = null;
+let bookIndex = null;
 
 function Book(name, author, pages, readStatus) {
   this.name = name;
@@ -36,12 +38,14 @@ function clearField() {
   [...bookInputs].forEach((x) => (x.value = ""));
 }
 function displayBooks() {
-  myLibrary.forEach((x) => {
-    console.log(x);
+  myLibrary.forEach((book, bookIndex) => {
+    console.log(book);
+    console.log(bookIndex);
     let bookCard = document.createElement("div");
     let bookHead = document.createElement("div");
     bookHead.classList.add("book-head");
     bookCard.classList.add("book");
+    bookCard.setAttribute("data-index", `${bookIndex}`);
     let bookPara = document.createElement("p");
     bookPara.textContent = "by ";
     let h2 = document.createElement("h2");
@@ -49,42 +53,57 @@ function displayBooks() {
     let separatorLine = document.createElement("div");
     separatorLine.classList.add("separator");
     trashIcon.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
-    h2.textContent = `${x.name}`;
+    h2.textContent = `${book.name}`;
     bookHead.appendChild(h2);
     bookHead.appendChild(trashIcon);
     bookCard.appendChild(bookHead);
     bookCard.appendChild(separatorLine);
     bookContainer.appendChild(bookCard);
     bookCard.insertBefore(bookPara, separatorLine);
+
+    // here iS the book Del button I am having trouble with
+    bookDelBtn = document.querySelectorAll(".book-head i");
+    console.log(bookDelBtn);
+    //
+
     let authorSpan = document.createElement("span");
-    authorSpan.textContent = `${x.author}`;
+    authorSpan.textContent = `${book.author}`;
     bookPara.appendChild(authorSpan);
+
     // book Length
     let bookLength = document.createElement("p");
     bookLength.textContent = "Length : ";
     bookLength.classList.add("book-stat");
     let bookLengthSpan = document.createElement("span");
-    bookLengthSpan.textContent = `${x.pages} pages`;
+    bookLengthSpan.textContent = `${book.pages} pages`;
     bookLength.appendChild(bookLengthSpan);
     bookCard.appendChild(bookLength);
+
     // book status
     let bookStatus = document.createElement("p");
     bookStatus.setAttribute("id", "book-status");
     bookStatus.textContent = "Status : ";
     bookStatus.classList.add("book-stat");
     let bookStatusSpan = document.createElement("span");
-    bookStatusSpan.textContent = x.readStatus ? "Done" : "In Progress";
+    bookStatusSpan.textContent = book.readStatus ? "Done" : "In Progress";
     bookStatus.appendChild(bookStatusSpan);
     bookCard.append(bookStatus);
     let syncIcon = document.createElement("div");
     syncIcon.innerHTML = '<i class="fas fa-sync"></i>';
     bookStatus.appendChild(syncIcon);
+
+    removeBookCard();
   });
 }
 function clearBookContainer() {
   while (bookContainer.childElementCount) {
     bookContainer.firstElementChild.remove();
   }
+}
+
+function deleteBook(arr, index) {
+  myLibrary.splice(index, 1);
+  return myLibrary;
 }
 
 addBookBtn.addEventListener("click", makeModalVisible);
@@ -95,3 +114,21 @@ submitFormBtn.addEventListener("click", () => {
   clearBookContainer();
   displayBooks();
 });
+
+function removeBookCard() {
+  [...bookDelBtn].forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      console.log("hi");
+      getBookIndex(e);
+      console.log("index :  " + bookIndex);
+      deleteBook(myLibrary, bookIndex);
+      clearBookContainer();
+      displayBooks();
+    })
+  );
+}
+
+function getBookIndex(e) {
+  bookIndex = e.target.parentElement.parentElement.parentElement.dataset.index;
+  return bookIndex;
+}
